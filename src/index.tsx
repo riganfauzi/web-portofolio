e2606f9f21cd2fc9696311b2cb4e4d02934ace9a
Rigan Nur Fauzi <riganfauzi@gmail.com>
2025-07-11 16:04:33 +0700

    feat: import aos

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Web } from "./screens/Web";
+import AOS from 'aos';
+
+AOS.init({
+  duration: 1000, 
+  easing: 'ease-in-out', 
+  once: true, 
+  mirror: false, 
+});
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
